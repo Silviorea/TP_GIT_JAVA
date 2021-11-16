99c94cab51ffb218bae1b394e8aa6ca264972375
Silvio REA <silvio.rea07@gmail.com>
2021-11-16 15:02:47 +0100

    add property age and getter/setter

--- src/fr/silvio/testgit/User.java
+++ src/fr/silvio/testgit/User.java
@@ -2,5 +2,31 @@ package fr.silvio.testgit;
 
 public class User
 {
+	
+	// Attributs
 	private String name;
+	private int age;
+	
+	
+	
+	
+	
+	// GETTER SETTER
+	public String getName()
+	{
+		return name;
+	}
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	public int getAge()
+	{
+		return age;
+	}
+	public void setAge(int age)
+	{
+		this.age = age;
+	}
+	
 }
